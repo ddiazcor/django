a1fa60ac6eec1034fbe65e41b1e294f900d4ff56
ddiazcor <ddiazcor@gmail.com>
2015-02-10 01:01:08 -0500

    configurado app ventas, ya quedo bien

--- demo/demo/apps/ventas/models.py
+++ demo/demo/apps/ventas/models.py
@@ -5,10 +5,15 @@ class cliente(models.Model):
     nombre    = models.CharField(max_length=200)
     apellidos = models.CharField(max_length=50)
     status    = models.BooleanField(default=True)
+    def __unicode__(self):
+        nombreCompleto = "%s %s"%(self.nombre,self.apellidos)
+        return nombreCompleto
 
 class producto(models.Model):
     nombre  = models.CharField(max_length=200)
     descripcion = models.TextField(max_length=200)
     status = models.BooleanField(default=True)
+    def __unicode__(self):
+        return self.nombre
 
 
