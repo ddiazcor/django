8d93f7a759411994bed8f9469c2e74ef055850b4
ddiazcor <ddiazcor@gmail.com>
2015-02-10 00:50:38 -0500

    configurando app ventas

--- demo/demo/apps/ventas/models.py
+++ demo/demo/apps/ventas/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class cliente(models.Model):
     nombre    = models.CharField(max_length=200)
     apellidos = models.CharField(max_length=50)
-    status    = models.BooleanField(default='true')
+    status    = models.BooleanField(default=True)
 
 class producto(models.Model):
     nombre  = models.CharField(max_length=200)
     descripcion = models.TextField(max_length=200)
-    status = models.BooleanField(default='true')
+    status = models.BooleanField(default=True)
 
 
