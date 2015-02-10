318b6f651ca33e26e270152472bb8bcaf99d363f
ddiazcor <ddiazcor@gmail.com>
2015-02-10 00:45:46 -0500

    configurando app ventas

--- demo/demo/apps/ventas/admin.py
+++ demo/demo/apps/ventas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from demo.apps.ventas.models import cliente, producto
+
+admin.site.register(cliente)
+admin.site.register(producto)
