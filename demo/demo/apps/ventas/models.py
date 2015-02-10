3e4f0084b0d29782a10f91b358326e20a9cc4013
ddiazcor <ddiazcor@gmail.com>
2015-02-10 00:03:39 -0500

    desarrollo e pychanr

---
+++ demo/demo/apps/ventas/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class cliente(models.Model):
+    nombre    = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=50)
+    status    = models.BooleanField(default=true)
+
+class producto(models.Model):
+    nombre  = models.CharField(max_length=200)
+    descripcion = models.TextField(max_length=200)
+    status = models.BooleanField(default=true)
+
+
