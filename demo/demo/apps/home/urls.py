e6ca5128d23f24e6650d5eb85932c5d5973625ea
ddiazcor <ddiazcor@gmail.com>
2015-02-10 23:50:34 -0500

    añadiendo una vista para la raiz

--- demo/demo/apps/home/urls.py
+++ demo/demo/apps/home/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns,url
+from django.conf.urls import patterns,url
 
 urlpatterns = patterns('demo.apps.home.views',
                        url(r'^$','index_view',name='vista_principal'),
--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'demo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('demo.apps.home.urls')),
+    url(r'^', include('demo.apps.home.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
