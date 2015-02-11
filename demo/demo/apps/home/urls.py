c9393821268ebe2c84682f402e1194915af78b6e
ddiazcor <ddiazcor@gmail.com>
2015-02-10 23:59:52 -0500

    añadiendo una vista para la raiz, home y about

--- demo/demo/apps/home/urls.py
+++ demo/demo/apps/home/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns,url
 
 urlpatterns = patterns('demo.apps.home.views',
                        url(r'^$','index_view',name='vista_principal'),
+                       url(r'^about/$','about_view',name='vista_about'),
 )
